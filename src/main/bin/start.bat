dde61ad075b4c23ce1ea2da8e8886e3ca14e89c4
Fractaliste <Fractaliste@users.noreply.github.com>
2017-10-27 11:10:35 +0100

    Amélioration du script de start

--- src/main/bin/start.bat
+++ src/main/bin/start.bat
@@ -1,1 +1,3 @@
-java -jar jetty-runner-${jetty.version}.jar --classes configuration ${artifactId}-${version}.${extension}
+java -jar jetty-runner-${jetty.version}.jar --log log/timing_yyyy_mm_dd.log^
+ --out log/app_yyyy_mm_dd.log --port ${jetty.port} --classes configuration^
+ ${artifactId}-${version}.war
